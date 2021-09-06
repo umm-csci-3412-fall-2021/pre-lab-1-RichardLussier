38eec5950ed5bc03f709662acc7cc32aaff5a281
RichardLussier <lussi036@morris.umn.edu>
2021-09-06 14:53:20 -0500

    Add comments to regex.sh
    This commit added comments to the line of code used in creating r1_output.txt to give a better understanding as to how it works. Additionally, this commit changes the wording in the comments left for the line concerning r0_output.txt to show sample output in a clearer manner.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,5 +1,19 @@
-#Takes the text inside of r0_input.txt and looks for a "<word>, <word>" pattern and puts it into an array called groups
-#It will then output a file containing "1. <first word> <new line> 2. <second word> <new line>"
+#Takes the text inside of r0_input.txt and looks for a "<first word>, <second word>" pattern and puts it into an array called groups.
+#It will then output a file r0_output.txt that organizes all found words into the pattern:
+
+#1. <first word>
+#2. <second word>
+#
+
+#and will continue this for all found patterns
 awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt
 
+#Takes the text inside of r1_input.txt and looks for "I am <word beginning with uppercase>. My favorite sandwhich is <word>." and stores the unknown words into an array called groups.
+#Then, it will output a file r1_output.txt that organizes these found words in the format:
+
+#1. <word beginning with uppercase>
+#2. <word>
+#
+
+#and will continue this for all found patterns
 awk 'match($0, /I am ([A-Z].+)\. My favorite sandwich is ([a-z]+)\./, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt > r1_output.txt
