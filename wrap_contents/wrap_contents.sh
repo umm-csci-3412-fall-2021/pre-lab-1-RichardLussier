1371e6180a29c52fa4ff4565349205a561053a31
RichardLussier <lussi036@morris.umn.edu>
2021-09-06 13:22:44 -0500

    Add comments to wrap_contents.sh
    This commit adds comments inside of the wrap_contents.sh file so there is a better understanding of what is happening.

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+#Adding helpful variable names to the given arguments
 fileName="$1"
 specifier="$2"
 resultFile="$3"
+
+#Creating more variable names by using the previously given arguments
 HEADER="$specifier"_header.html
 FOOTER="$specifier"_footer.html
+
+#Concatenating the three files together into the resultFile
 cat "$HEADER" "$fileName" "$FOOTER" > "$resultFile"
